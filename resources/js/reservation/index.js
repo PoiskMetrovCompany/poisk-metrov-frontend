751e798456c3a6456ba4298050b307726447823c
VyacheslavMironov <vuacheslav.mir@gmail.com>
2025-03-04 18:38:52 +0700

    Промежуточный коммит: переверстал список жк внёс небольшие правки

--- resources/js/reservation/index.js
+++ resources/js/reservation/index.js
@@ -1,5 +1,17 @@
 const elementButtonAddCoBorrower = document.getElementById('btn-add_co_borrower');
+const elementDeleteBorrower = document.getElementById('delete_borrower');
 
+/**
+ * добавление формы для заполнения "созаёмщика"
+ */
 elementButtonAddCoBorrower.addEventListener('click', function () {
     document.querySelector('.co-borrower__card-body').style.display = 'block';
+});
+
+/**
+ * удаление формы "созаёмщика" + очищение полей
+ */
+elementDeleteBorrower.addEventListener('click', function () {
+    document.querySelector('.co-borrower__card-body').style.display = 'none';
+    // TODO: очистить все поля
 });
