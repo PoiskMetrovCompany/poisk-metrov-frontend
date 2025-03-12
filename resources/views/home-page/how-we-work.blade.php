17e8a7e6bd086876f1eeda40cd8819f71f7b1286
VyacheslavMironov <vuacheslav.mir@gmail.com>
2025-03-12 18:03:10 +0700

    Промежуточный коммит: вынес данные в логику представления

--- resources/views/home-page/how-we-work.blade.php
+++ resources/views/home-page/how-we-work.blade.php
@@ -1,9 +1,29 @@
+@php
+$title = 'Как мы работаем?';
+// 
+$galleryList = [
+    [
+        'title' => 'Экскурсии по новостройкам',
+        'description' => 'Раскрывать потенциал каждой локации, делая жизнь в доме источником положительных эмоций',
+        'image' => '/placeholders/placeholder-24.jpeg'
+    ],
+    [
+        'title' => 'Только актуальные цены',
+        'description' => 'Данные на сайте обновляются ежедневно, чтобы вы могли быть уверены в их достоверности.',
+        'image' => '/placeholders/placeholder-25.jpeg'
+    ],
+    [
+        'title' => 'Недвижимость без комиссий',
+        'description' => 'Мы ценим ваше время и финансовые ресурсы, поэтому предлагаем прозрачные условия покупки.',
+        'image' => '/placeholders/placeholder-26.jpeg'
+    ],
+];
+@endphp
+
 <div class="how-work-gallery work-base-container">
     @vite('resources/js/homePage/howWeWork.js')
     <div class="title-flex">
-        <div class="title">
-            Как мы работаем?
-        </div>
+        <div class="title">{{ $title }}</div>
     </div>
     <div class="how-work-gallery work-grid">
         <div class="how-work-gallery gallery-with-buttons">
@@ -12,24 +32,20 @@
                 @include('buttons.arrow-right')
             </div>
             <div id="aboutus-gallery" class="how-work-gallery images">
-                <img class="how-work-gallery work-container" src="/placeholders/placeholder-24.jpeg" />
-                <img class="how-work-gallery work-container" src="/placeholders/placeholder-25.jpeg" />
-                <img class="how-work-gallery work-container" src="/placeholders/placeholder-26.jpeg" />
+                @foreach($galleryList as $gallery)
+                    <img class="how-work-gallery work-container"
+                         src="{{ $gallery['image'] }}"
+                         alt="{{ $gallery['title'] }}" />
+                @endforeach
             </div>
         </div>
         <div class="how-work-gallery texts">
-            <div class="how-work-gallery work-title">
-                <div>Экскурсии по новостройкам</div>
-                Раскрывать потенциал каждой локации, делая жизнь в доме источником положительных эмоций
-            </div>
-            <div class="how-work-gallery work-title" style="display: none">
-                <div>Только актуальные цены</div>
-                Данные на сайте обновляются ежедневно, чтобы вы могли быть уверены в их достоверности.
-            </div>
-            <div class="how-work-gallery work-title" style="display: none">
-                <div>Недвижимость без комиссий</div>
-                Мы ценим ваше время и финансовые ресурсы, поэтому предлагаем прозрачные условия покупки.
-            </div>
+            @foreach($galleryList as $gallery)
+                <div class="how-work-gallery work-title">
+                    <div>{{ $gallery['title'] }}</div>
+                    {{ $gallery['description'] }}
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
