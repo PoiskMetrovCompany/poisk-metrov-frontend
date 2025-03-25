7fe9557ac3d0aff8289ac4b2d51f1a88fb02358b
VyacheslavMironov <vuacheslav.mir@gmail.com>
2025-03-25 17:59:34 +0700

    Проверка имени пдф файла

--- resources/js/downloadFile.js
+++ resources/js/downloadFile.js
@@ -5,9 +5,11 @@ export function downloadFile(url, fileName) {
       const aElement = document.createElement('a');
       aElement.setAttribute('download', fileName);
       const href = URL.createObjectURL(res);
+        console.log(href)
       aElement.href = href;
       aElement.setAttribute('target', '_blank');
       aElement.click();
+      console.log(aElement)
       URL.revokeObjectURL(href);
     });
 };
