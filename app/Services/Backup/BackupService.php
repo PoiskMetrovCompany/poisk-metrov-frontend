304dc7f61ae5b7f9cddd721bb2013c04e236bdf4
VyacheslavMironov <vuacheslav.mir@gmail.com>
2025-04-04 10:17:07 +0700

    Поправил название архива

--- app/Services/Backup/BackupService.php
+++ app/Services/Backup/BackupService.php
@@ -106,7 +106,7 @@ final class BackupService implements BackupServiceInterface
     public function execute(): void
     {
         $depthPath = 3;
-        $toDate = date('Y-m-d-H-i-s');
+        $toDate = date('d-m-y');
         $filename = "{$toDate}_.tar";
         $archiveDir = dirname(__DIR__, $depthPath);
         $archivePath = "{$archiveDir}/{$filename}";
