162b71b92e8c11541e4a4b69592e938535a0f55b
VyacheslavMironov <vuacheslav.mir@gmail.com>
2025-04-10 16:30:27 +0700

    Промежуточный коммит: сделал переход на внутреннюю страницу жк из описания карточки жк, добавил якорную ссылку на тип квартиры, обнаружил баг в "RealEstateService"

--- resources/views/custom-elements/building-card/line.blade.php
+++ resources/views/custom-elements/building-card/line.blade.php
@@ -1,8 +1,28 @@
 @isset($data[$num])
     <li>
-        <div type="subheader">{{ $data[$num]['name'] }}</div>
-        <div>от {{ $data[$num]['min-square'] }} м²</div>
-        <div>от {{ $data[$num]['min-price'] }}</div>
+        @php
+            $link = '#';
+
+            if ($data[$num]['name'] === 'студ.') $link= '#apartment_type_study';
+            if ($data[$num]['name'] === '1к. кв.') $link= '#apartment_type_one';
+            if ($data[$num]['name'] === '2к. кв.') $link= '#apartment_type_two';
+            if ($data[$num]['name'] === '3к. кв.') $link= '#apartment_type_free';
+            if ($data[$num]['name'] === '4к. кв.') $link= '#apartment_type_foo';
+            if ($data[$num]['name'] === '5к. кв.') $link= '#apartment_type_five';
+            if ($data[$num]['name'] === '6к. кв.') $link= '#apartment_type_six';
+        @endphp
+        <a class="apartment-type-card-item" href="/{{$code}}/{{$link}}" style="
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
+            justify-content: space-around;
+            align-items: center;
+            width: 21rem;
+            gap: 10px;
+        ">
+            <div type="subheader">{{ $data[$num]['name'] }}</div>
+            <div>от {{ $data[$num]['min-square'] }} м²</div>
+            <div>от {{ $data[$num]['min-price'] }}</div>
+        </a>
     </li>
 @else
     <li>
