6767b29a76593b31b590b1f27d2cc854af7d7fb7
VyacheslavMironov <vuacheslav.mir@gmail.com>
2025-04-14 15:43:51 +0700

    Сделал анимацию раскрытия списка ЖК и поправил закрытие выбранных квартир

--- resources/js/anchorLink.js
+++ resources/js/anchorLink.js
@@ -1,27 +1,42 @@
-if (window.location.hash) {
-
-    const linkParse = decodeURIComponent(window.location.hash.substring(1));
-    const dropdownHeaders = document.querySelectorAll('#type_apartment');
-    const anchors = Array.from(dropdownHeaders).map(el => el.innerHTML.trim());
-
-    document.querySelector('.anchor').setAttribute('id', linkParse);
-    let anchorIndex = null;
-    for (let i = 0; i < anchors.length; i++)
-    {
-        if (anchors[i] === linkParse) {
-            anchorIndex = i;
-
-            const dropdownHeaderList = document.querySelectorAll('#type_apartment');
-            dropdownHeaderList.forEach((el) => {
-                el.querySelector('#type_apartment');
-
-                if (el.innerHTML.trim() === anchors[i]) {
-                    try {
-                        document.querySelector(`#apartment-dropdown-${[anchorIndex]}`).classList.add('open');
-                    } catch (TypeError) {}
-
-                }
+document.addEventListener('DOMContentLoaded', () => {
+    function scrollToAnchor() {
+        const anchorElement = document.querySelector('.anchor');
+        if (anchorElement) {
+            anchorElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
             });
         }
     }
-}
+
+    function action() {
+        if (window.location.hash) {
+            const linkParse = decodeURIComponent(window.location.hash.substring(1));
+            const dropdownHeaders = document.querySelectorAll('#type_apartment');
+            const anchors = Array.from(dropdownHeaders).map(el => el.innerHTML.trim());
+
+            const anchorElement = document.querySelector('.anchor');
+            if (anchorElement) {
+                anchorElement.setAttribute('id', linkParse);
+            }
+
+            const anchorIndex = anchors.findIndex(anchor => anchor === linkParse);
+            if (anchorIndex !== -1) {
+                try {
+                    const dropdown = document.querySelector(`#apartment-dropdown-${anchorIndex}`);
+                    if (dropdown) {
+                        const dropdownHeaderElement = dropdown.previousElementSibling;
+                        setTimeout(
+                            () => { dropdownHeaderElement.click() },
+                            1500
+                        );
+                    }
+                } catch (error) { }
+            }
+
+            scrollToAnchor();
+        }
+    }
+
+    action();
+});
