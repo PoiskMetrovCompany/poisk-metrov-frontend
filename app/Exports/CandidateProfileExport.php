852c2c35529a0aef12c873af623183d14b2c7e4d
VuacheslavMironov <vuacheslav.mir@gmail.com>
2025-07-21 18:13:40 +0700

    Промежуточный коммит: поправил экспорт в .xlsx

--- app/Exports/CandidateProfileExport.php
+++ app/Exports/CandidateProfileExport.php
@@ -18,7 +18,7 @@ class CandidateProfileExport implements FromCollection, WithHeadings, WithEvents
      */
     protected static array $columns = [
         'candidate_profiles.status',
-        'vacancies.title',
+        'vacancies.title AS vacancy_title',
         'candidate_profiles.last_name',
         'candidate_profiles.first_name',
         'candidate_profiles.middle_name',
@@ -36,14 +36,16 @@ class CandidateProfileExport implements FromCollection, WithHeadings, WithEvents
         'candidate_profiles.permanent_registration_address',
         'candidate_profiles.temporary_registration_address',
         'candidate_profiles.actual_residence_address',
-        'marital_statuses.title',
-//        'candidate_profiles.family_partner',
-//        'candidate_profiles.adult_family_members',
-//        'candidate_profiles.adult_children',
-//        'candidate_profiles.serviceman',
+        'marital_statuses.title AS marital_status_title',
+        'family_partner',
+        'family_partner_age',
+        'family_partner_relation',
+        'adult_family_members_list',
+        'adult_children_list',
+        'candidate_profiles.serviceman',
+        'candidate_profiles.is_data_processing',
         'candidate_profiles.law_breaker',
         'candidate_profiles.legal_entity',
-//        'candidate_profiles.is_data_processing',
         'candidate_profiles.comment',
     ];
 
@@ -52,17 +54,77 @@ class CandidateProfileExport implements FromCollection, WithHeadings, WithEvents
     */
     public function collection()
     {
-        return Db::table('candidate_profiles')
-            ->select(array_map(function ($column) {
-                if ($column === 'candidate_profiles.birth_date' || $column === 'birth_date') {
-                    return DB::raw("DATE_FORMAT(candidate_profiles.birth_date, '%d.%m.%Y') as birth_date");
-                }
-                return $column;
-            }, self::$columns))
+        $query = DB::table('candidate_profiles')
             ->join('vacancies', 'vacancies.key', '=', 'candidate_profiles.vacancies_key')
-            ->join('marital_statuses', 'marital_statuses.key', '=', 'candidate_profiles.marital_statuses_key')
+            ->join('marital_statuses', 'marital_statuses.key', '=', 'candidate_profiles.marital_statuses_key');
+
+        $realColumns = array_filter(self::$columns, function ($column) {
+            return !in_array($column, [
+                'family_partner',
+                'family_partner_age',
+                'family_partner_relation',
+                'adult_family_members_list',
+                'adult_children_list',
+            ]);
+        });
+
+        $query->select($realColumns);
+        $query->selectRaw("
+            COALESCE(JSON_UNQUOTE(JSON_EXTRACT(candidate_profiles.family_partner, '$.name')), '') AS family_partner,
+            COALESCE(JSON_UNQUOTE(JSON_EXTRACT(candidate_profiles.family_partner, '$.age')), '') AS family_partner_age,
+            COALESCE(JSON_UNQUOTE(JSON_EXTRACT(candidate_profiles.family_partner, '$.relation')), '') AS family_partner_relation,
+
+            COALESCE(
+                GROUP_CONCAT(JSON_UNQUOTE(JSON_EXTRACT(candidate_profiles.adult_family_members, '$[*].name')) SEPARATOR ', '),
+                ''
+            ) AS adult_family_members_list,
+
+            COALESCE(
+                GROUP_CONCAT(JSON_UNQUOTE(JSON_EXTRACT(candidate_profiles.adult_children, '$[*].name')) SEPARATOR ', '),
+                ''
+            ) AS adult_children_list
+        ");
+
+        $groupByColumns = [
+            'candidate_profiles.id',
+            'vacancies.title',
+            'marital_statuses.title',
+            'candidate_profiles.status',
+            'candidate_profiles.last_name',
+            'candidate_profiles.first_name',
+            'candidate_profiles.middle_name',
+            'candidate_profiles.reason_for_changing_surnames',
+            'candidate_profiles.birth_date',
+            'candidate_profiles.country_birth',
+            'candidate_profiles.city_birth',
+            'candidate_profiles.mobile_phone_candidate',
+            'candidate_profiles.home_phone_candidate',
+            'candidate_profiles.mail_candidate',
+            'candidate_profiles.inn',
+            'candidate_profiles.passport_series',
+            'candidate_profiles.passport_number',
+            'candidate_profiles.passport_issued',
+            'candidate_profiles.permanent_registration_address',
+            'candidate_profiles.temporary_registration_address',
+            'candidate_profiles.actual_residence_address',
+            'candidate_profiles.law_breaker',
+            'candidate_profiles.legal_entity',
+            'candidate_profiles.comment',
+            'candidate_profiles.serviceman',
+            'candidate_profiles.is_data_processing',
+            'candidate_profiles.updated_at',
+        ];
+
+        $query->groupBy($groupByColumns);
+        $data = $query
             ->orderBy('candidate_profiles.updated_at', 'desc')
             ->get();
+
+        return $data->map(function ($item) {
+            $item->serviceman = isset($item->serviceman) ? ($item->serviceman ? 'Да' : 'Нет') : 'Нет';
+            $item->is_data_processing = isset($item->is_data_processing) ? ($item->is_data_processing ? 'Да' : 'Нет') : 'Нет';
+            return $item;
+        });
     }
 
     public function headings(): array
@@ -88,13 +150,15 @@ class CandidateProfileExport implements FromCollection, WithHeadings, WithEvents
             'Временный адрес проживания',
             'Текущий адрес проживания',
             'Статус семейного положения',
-//        'candidate_profiles.family_partner',
-//        'candidate_profiles.adult_family_members',
-//        'candidate_profiles.adult_children',
-//        'candidate_profiles.serviceman',
+            'Супруг(а) - Имя',
+            'Супруг(а) - Возраст',
+            'Супруг(а) - Отношение',
+            'Совершеннолетние члены семьи (ФИО)',
+            'Совершеннолетние дети (ФИО)',
+            'Служит ли в ВС/МВД/ФСБ?',
+            'Согласие на обработку данных?',
             'Причины нарушения закона',
             'Юридический статус',
-//        'candidate_profiles.is_data_processing',
             'Комментарий',
         ];
     }
@@ -103,9 +167,7 @@ class CandidateProfileExport implements FromCollection, WithHeadings, WithEvents
     {
         return [
             AfterSheet::class => function(AfterSheet $event) {
-//                $event->sheet->getSheet()->freezePane('A2');
-
-                foreach (range('A', 'W') as $col) {
+                foreach (range('A', 'AD') as $col) {
                     $event->sheet->getDelegate()
                         ->getColumnDimension($col)
                         ->setAutoSize(true);
