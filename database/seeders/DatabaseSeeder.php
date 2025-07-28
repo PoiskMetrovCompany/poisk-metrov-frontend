529dc79562e3f52d86d69c3fb2fbe73123021df0
vuacheslavMironov <vuacheslav.mir@gmail.com>
2025-07-28 14:31:29 +0700

    Промежуточный коммит: пофиксил сидер

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ReservationSeeder::class,
+//            ReservationSeeder::class,
             VacancySeeder::class,
             MaritalStatusSeeder::class,
-            CandidateProfileSeeder::class,
+            //CandidateProfileSeeder::class,
         ]);
     }
 }
