3c4ace6205834de106f0f8447d6745089dbb29de
vuacheslavMironov <vuacheslav.mir@gmail.com>
2025-07-30 17:27:03 +0700

    Поправил MaritalStatusSeeder

--- database/seeders/MaritalStatusSeeder.php
+++ database/seeders/MaritalStatusSeeder.php
@@ -6,6 +6,7 @@ use App\Models\MaritalStatuses;
 use App\Models\Vacancies;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class MaritalStatusSeeder extends Seeder
 {
@@ -14,7 +15,10 @@ class MaritalStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        MaritalStatuses::truncate();
-        MaritalStatuses::factory()->count(3)->create();
+//        MaritalStatuses::truncate();
+//        MaritalStatuses::factory()->count(3)->create();
+        MaritalStatuses::created(['key' => Str::uuid()->toString(), 'title' => 'Состою в зарегистрированном браке']);
+        MaritalStatuses::created(['key' => Str::uuid()->toString(), 'title' => 'Состою в незарегистрированном браке']);
+        MaritalStatuses::created(['key' => Str::uuid()->toString(), 'title' => 'Не состою в браке']);
     }
 }
