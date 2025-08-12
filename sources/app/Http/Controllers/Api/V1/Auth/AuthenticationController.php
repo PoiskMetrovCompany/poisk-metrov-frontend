afc4a52c5d8e8d3d3b81c6f326a9bc62c4e37320
VuacheslavMironov <vuacheslav.mir@gmail.ru>
2025-08-13 01:06:18 +0700

    Промежуточный коммит: сделал авторизацию через апи

---
+++ sources/app/Http/Controllers/Api/V1/Auth/AuthenticationController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Core\Abstracts\AbstractOperations;
+use App\Core\Common\CallUrlEnum;
+use App\Core\Interfaces\Repositories\AuthorizationCallRepositoryInterface;
+use App\Core\Interfaces\Repositories\UserAdsAgreementRepositoryInterface;
+use App\Core\Interfaces\Repositories\UserRepositoryInterface;
+use App\Core\Interfaces\Services\CallServiceInterface;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Accounts\AccountLoginRequest;
+use App\Http\Requests\ConfirmUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\AuthorizationCall;
+use App\Models\User;
+use App\Services\AdsAgreementService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+/**
+ * @see AppServiceProvider::registerAdsAgreementService()
+ * @see AppServiceProvider::registerUserAdsAgreementRepository()
+ * @see AppServiceProvider::registerUserRepository()
+ * @see AppServiceProvider::registerAuthorizationCallRepository()
+ * @see AdsAgreementServiceInterface
+ * @see UserAdsAgreementRepositoryInterface
+ * @see UserRepositoryInterface
+ * @see AuthorizationCallRepositoryInterface
+ */
+class AuthenticationController extends AbstractOperations
+{
+    private string $campaignId;
+    private string $apiKey;
+    private string $flashCallURL;
+    private string $callPhoneURL;
+
+    /**
+     * @param UserRepositoryInterface $userRepository
+     * @param UserAdsAgreementRepositoryInterface $adsAgreementRepository
+     * @param AdsAgreementService $adsAgreementService
+     * @param CallServiceInterface $callService
+     * @param AuthorizationCallRepositoryInterface $authorizationCallRepository
+     */
+    public function __construct(
+        protected UserRepositoryInterface $userRepository,
+        protected UserAdsAgreementRepositoryInterface $adsAgreementRepository,
+        protected AdsAgreementService $adsAgreementService,
+        protected CallServiceInterface $callService,
+        protected AuthorizationCallRepositoryInterface $authorizationCallRepository,
+    )
+    {
+        $this->apiKey = config('call.api_key');
+        $this->campaignId = config('call.campaing_id');
+        $this->flashCallURL = CallUrlEnum::FLASH_CALL_URL->value;
+        $this->callPhoneURL = CallUrlEnum::CALL_PHONE_URL->value;
+    }
+
+    /**
+     * @OA\Post(
+     * tags={"Auth"},
+     * path="/api/v1/auth/authentication/",
+     * summary="Определение аккаунта (сайт)",
+     * description="Возвращение JSON объекта",
+     * @OA\RequestBody(
+     * required=true,
+     * @OA\JsonContent(
+     * @OA\Property(property="phone", type="string", example="+7 (993) 952-00-85")
+     * )
+     * ),
+     * @OA\Response(
+     * response=201,
+     * description="УСПЕХ!",
+     * @OA\JsonContent(
+     * @OA\Property(property="phone", type="string", example="+7 (993) 952-00-85")
+     * )
+     * ),
+     * @OA\Response(
+     * response=404,
+     * description="Resource not found",
+     * @OA\JsonContent(
+     * @OA\Property(property="error", type="string", example="Resource not found")
+     * )
+     * )
+     * )
+     *
+     * @param ConfirmUserRequest $request
+     * @return JsonResponse
+     */
+    public function __invoke(ConfirmUserRequest $request)
+    {
+        $phone = $request->validated('phone');
+
+        $isUserAds = $this->adsAgreementRepository->findByPhone($phone);
+
+        if ($isUserAds == null) {
+            $this->adsAgreementService->setAdsAgreement($phone, null);
+        }
+
+        $fields['phone'] = $phone;
+        $fields['pincode'] = $this->callService->generateRandomVerificationCode();
+
+        $response = $this->callService->sendRequest($this->apiKey, $this->campaignId, $this->flashCallURL, $fields);
+        if ($response == null) {
+            return new JsonResponse(
+                data: [
+                    ...self::identifier(),
+                    'attributes' => ['data' => 'No response'],
+                    ...self::metaData($request, $request->all()),
+                ],
+                status: Response::HTTP_OK
+            );
+        }
+
+        if (is_string($response->data)) {
+            return new JsonResponse(
+                data: [
+                    ...self::identifier(),
+                    ...self::attributes(['data' => $response->data]),
+                    ...self::metaData($request, $request->all()),
+                ],
+                status: Response::HTTP_OK
+            );
+        }
+
+        //TODO: вынести в сервис наверное
+        $phone = $request->validated('phone');
+        $pincode = $request->validated('pincode');
+        $call = AuthorizationCall::where('pincode', $pincode)->where('phone', $phone)->first();
+        // TODO: END
+
+        $data = [
+            'phone' => $phone,
+            'pincode' => $response->data->pincode,
+            'call_id' => $response->data->call_id
+        ];
+
+        if ($call == null) {
+            $this->authorizationCallRepository->store($data);
+        } else {
+            $call->update($data);
+            $call->save();
+        }
+
+        return new JsonResponse(
+            data: [
+                ...self::identifier(),
+                ...self::attributes([]),
+                ...self::metaData($request, $request->all()),
+            ],
+            status: Response::HTTP_OK
+        );
+    }
+
+    public function getEntityClass(): string
+    {
+        return User::class;
+    }
+
+    public function getResourceClass(): string
+    {
+        return UserResource::class;
+    }
+}
