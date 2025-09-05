d25425b00df2ec4ea3d9d2a17ef1745bf592ea15
Justikarx <twin.m999@gmail.com>
2025-09-05 15:10:06 +0700

    Фикс запроса

---
+++ sources/database/migrations/2025_09_05_072359_add_missing_fields_to_candidate_profiles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('candidate_profiles', function (Blueprint $table) {
+            $table->string('work_team', 255)->nullable()->after('marital_statuses_key');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('candidate_profiles', function (Blueprint $table) {
+            $table->dropColumn('work_team');
+        });
+    }
+};
